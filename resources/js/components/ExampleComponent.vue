49686c9a6acb4165d20adc22468909007a7cd826
Andre Madarang <andre_madarang@hotmail.com>
2021-09-29 13:15:53 -0400

    Headless UI working

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,12 +1,35 @@
 <template>
-  <div>Example Component, {{ greeting }}</div>
-  <div>Foo: {{ foo }}</div>
+  <Menu>
+    <MenuButton>More</MenuButton>
+    <MenuItems>
+      <MenuItem v-slot="{ active }">
+        <a :class='{ "bg-blue-500": active }' href="/account-settings">
+          Account settings
+        </a>
+      </MenuItem>
+      <MenuItem v-slot="{ active }">
+        <a :class='{ "bg-blue-500": active }' href="/account-settings">
+          Documentation
+        </a>
+      </MenuItem>
+      <MenuItem disabled>
+        <span class="opacity-75">Invite a friend (coming soon!)</span>
+      </MenuItem>
+    </MenuItems>
+  </Menu>
 </template>
 
 <script>
 import { ref } from 'vue'
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue"
 
 export default {
+    components: {
+      Menu,
+      MenuButton,
+      MenuItems,
+      MenuItem,
+    },
     props: ['foo'],
     setup() {
         const greeting = ref('hello there')
